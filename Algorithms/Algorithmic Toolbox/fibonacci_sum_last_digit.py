3a9a110a9547097ddfbbe24bea6530b2750f4829
Chiemezie Jacob <Chiemezie.jacob@gmail.com>
2022-08-10 19:14:58 +0100

    Fibonnaci again

---
+++ Algorithms/Algorithmic Toolbox/fibonacci_sum_last_digit.py
@@ -0,0 +1,36 @@
+# Uses python3
+import sys
+
+def fibonacci_sum_naive(n):
+    if n <= 1:
+        return n
+
+    previous = 0
+    current  = 1
+    _sum      = 1
+
+    for _ in range(n - 1):
+        previous, current = current, previous + current
+        _sum += current
+
+    return _sum % 10
+
+
+def fibonacci_sum_fast(n):
+    arr = [0, 1]
+    for i in range(2, n + 1):
+        arr.append(0)
+    sum = 1
+    for i in range(2, n + 1):
+        arr[i] = (arr[i-1] + arr[i-2]) % 10
+        sum += arr[i]
+
+    return sum % 10
+
+# print(fibonacci_sum_fast(100))
+    
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    n = int(input)
+    print(fibonacci_sum_fast(n))
